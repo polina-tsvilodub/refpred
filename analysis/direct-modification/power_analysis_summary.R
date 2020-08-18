9f1a38eea4af1a8776a1f7180eb91259b2e2ba33
polina-tsvilodub <polina.tsvilodub@gmail.com>
2020-08-18 18:35:25 +0200

    plot power against nsims

--- analysis/direct-modification/power_analysis_summary.R
+++ analysis/direct-modification/power_analysis_summary.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(foreach)
 # change to results folder
 setwd("results/sim_100")
 # bind together all summary files
@@ -13,7 +14,7 @@ full_data_400 <- list.files(
                        ) %>%
   map_df(~read_csv(.))
 
-full_data_500 <- rbind(full_data_100, full_data_400)
+full_data_500 <- rbind(full_data_100, full_data_400) 
 
 full_data_500_summary <- full_data_500 %>%
   filter(key == "syntax_critical") %>%
@@ -22,4 +23,30 @@ full_data_500_summary <- full_data_500 %>%
   summarise(power_syntax = round(mean(check_syntax), 3))
 
 write_csv(full_data_500_summary, "../direct_mod_power_analysis_fullData_3000iter_500sim_summary.csv")
+
+# plot power as n iter increases
+n.sims = seq(50, 500, by=50) 
+
+get_intermediate_power = function(N) {
+  summary <-  full_data_500 %>% 
+    filter(!(n.subj %in% c(50, 100))) %>%
+    group_by(n.subj) %>% slice(1:(10*N)) %>%
+    filter(key == "syntax_critical") %>%
+    mutate(check_syntax = ifelse((lower > 0) | (upper < 0 ), 1, 0)
+    ) %>%
+    group_by(n.subj) %>%
+    summarise(power_syntax = round(mean(check_syntax), 3)) %>% mutate(n.sim=N) 
+}
+
+my_powers <- foreach(i = n.sims, .combine=rbind) %do% {get_intermediate_power(i)}  
+
+my_powers <- my_powers %>% mutate(n.subj = as.factor(n.subj))
+ggplot(my_powers, aes(x = n.sim, y = power_syntax, color = n.subj) ) +
+  geom_point() +
+  geom_line() +
+  ylab("Power") +
+  ylim(0.6, 1) +
+  xlab("Number of simulations") +
+  ggtitle("Power as a function of number of simulations")
+ggsave("../power_analysis_3000iter_500sim_plot.pdf", width = 5, height = 4)
 
