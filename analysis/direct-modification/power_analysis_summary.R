d4b9cdfa0ff34813131875e7da685906a209834c
polina-tsvilodub <polina.tsvilodub@gmail.com>
2020-08-18 17:47:55 +0200

    summarise 500sim power analysis

--- analysis/direct-modification/power_analysis_summary.R
+++ analysis/direct-modification/power_analysis_summary.R
@@ -1,9 +1,25 @@
 library(tidyverse)
 # change to results folder
-setwd("results/")
+setwd("results/sim_100")
 # bind together all summary files
-data <- list.files(
-                    pattern = "*summary.csv"
+full_data_100 <- list.files(
+                    pattern = "direct_mod_power_analysis_fullData_final*"
                    ) %>%
-  map_df(~read_csv(.)) %>%
-  write_csv(., "direct_mod_power_analysis_grand_summary.csv")
+  map_df(~read_csv(.))
+
+
+full_data_400 <- list.files(
+                       pattern = "direct_mod_power_analysis_fullData_final*"
+                       ) %>%
+  map_df(~read_csv(.))
+
+full_data_500 <- rbind(full_data_100, full_data_400)
+
+full_data_500_summary <- full_data_500 %>%
+  filter(key == "syntax_critical") %>%
+  mutate(check_syntax = ifelse((lower > 0) | (upper < 0 ), 1, 0)) %>%
+  group_by(n.subj) %>%
+  summarise(power_syntax = round(mean(check_syntax), 3))
+
+write_csv(full_data_500_summary, "../direct_mod_power_analysis_fullData_3000iter_500sim_summary.csv")
+
