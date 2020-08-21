7a69bbae6ad3dded862103fcc80cd86a68c15d32
polina-tsvilodub <polina.tsvilodub@gmail.com>
2020-08-21 20:46:10 +0200

    add diff N items

--- analysis/direct-modification/power_analysis.R
+++ analysis/direct-modification/power_analysis.R
@@ -6,13 +6,18 @@ library(broom)
 
 # list of subject-Ns; argument passed is the index accessing the respective N
 subjN_list <- c(80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300)
+Nitems_list <- c(5, 6, 8, 10)
 
 # get the argument passed (assuming the index is the first one after --args)
-arg <- commandArgs(trailingOnly = T)[1]
-arg <- as.numeric(arg)
+subjN <- commandArgs(trailingOnly = T)[1]
+itemN <- commandArgs(trailingOnly = T)[2]
+subjN <- as.numeric(subjN)
+itemN <- as.numeric(itemN)
 # get the N subj at the position in list corresponding the passed index
-currentSubj.N <- subjN_list[arg]
-print(currentSubj.N)
+currentSubj.N <- subjN_list[subjN]
+currentItem.N <- Nitems_list[itemN]
+print(paste("Number of subjects", currentSubj.N, sep=" "))
+print(paste("Number of items", currentItem.N, sep=" "))
 
 # add appropriate path
 pilot_data <- read_csv("../../data/direct-modification/results_all_pilots_tidy.csv")
@@ -71,20 +76,20 @@ predicted_fit <- brm(
 
 
 # helper function to get 4 out of 5 possible target pairs per participant 
-get_targets <- function(N){
-  targets <- sample(c("sunflower_dandelion", "redwood_bonsai", "doberman_chihuahua", "Great Dane_pug", "eagle_hummingbird"), 4, replace = F)
+get_targets <- function(N, items){
+  targets <- sample(c("sunflower_dandelion", "redwood_bonsai", "doberman_chihuahua", "Great Dane_pug", "eagle_hummingbird", "item1_item2", "item3_item4", "item5_item6", "item7_item8", "item9_item10")[1:items], 4, replace = F)
   unlist(str_split(targets, pattern="_"))
 }
 
 # helper function to get posterior predictive draws for participant number N
-get_new_data <- function(N, pilot_model, pilot_data) {
+get_new_data <- function(N, pilot_model, pilot_data, items) {
   
   trials <- rep(c("critical", "filler"), each = 4)
   syntax <- rep(c("subj", "subj", "pred", "pred"), times = 2)
   
   new.data <- tibble(
     workerid = 1:N,
-    target = map(workerid, get_targets)
+    target = map(workerid, get_targets, items)
   ) %>%
     unnest(cols = target) %>%
     mutate(
@@ -94,23 +99,23 @@ get_new_data <- function(N, pilot_model, pilot_data) {
   
   data <- add_predicted_draws(model=pilot_model,
                               newdata = new.data,
-                              allow_new_levels = T, # allow sampling new workerids
+                              allow_new_levels = T, # allow sampling new workerids and new items
                               sample_new_levels = "gaussian", # sample new REs based on estimated REs
                               n = 1)
   return(data)
 }
 
 # create file path for streaming output
-stream_out <- paste("results/direct_mod_power_analysis_fullData_stream_", currentSubj.N, "subj_", n_iter,  "iter_", n_sim, "sim.csv", sep="")
+stream_out <- paste("results/direct_mod_power_analysis_fullData_stream_", currentSubj.N, "subj_", currentItem.N, "items_", n_iter,  "iter_", n_sim, "sim.csv", sep="")
 
 # simulate data and update model fit
-sim_data_fit <- function(seed, N) {
+sim_data_fit <- function(seed, N, items) {
   set.seed(seed)
-  print(paste("iteration:", seed, " , subjects:", N, sep=""))
+  print(paste("iteration:", seed, " , subjects:", N, "items:", items, sep=" "))
   
   # predictions are based on pilot data, i.e. grouped by original pilot input rows
   # corresponds to brms::predict.brmsfit()
-  data <- get_new_data(N, pilot_model = pilot_model, pilot_data = pilot_data) %>%
+  data <- get_new_data(N, pilot_model = pilot_model, pilot_data = pilot_data, items = items) %>%
     mutate(trial_dev = factor(trial_dev, levels = c("filler", "critical")),
            syntax_dev = factor(syntax_dev, levels = c("subj", "pred")))
 
@@ -147,35 +152,36 @@ sim_data_fit <- function(seed, N) {
     tibble() %>%
     mutate(
       n.subj = N,
+      n.item = items, 
       seed = seed
     ) %>%
     # write out results for each subject N and each seed
-    write_csv(., stream_out, append = T, col_names = (seed == 1)) %>% select(-n.subj, -seed)
+    write_csv(., stream_out, append = T, col_names = (seed == 1)) %>% select(-n.subj, -seed, -n.item)
 }
 
 # helper function iterating over seeds (=iterations of the simulation)
-sim.power <- function(n.subj, n.sim) {
+sim.power <- function(n.subj, n.sim, n.item) {
   print(n.subj)
   sim <- tibble( seed = 1:n.sim) %>%
     mutate(
-      tidy = map(seed, sim_data_fit, N = n.subj)
+      tidy = map(seed, sim_data_fit, N = n.subj, items = n.item)
     ) %>%
     unnest(tidy)
 }
 
 # iterate over subject number passed to script 
-analyse_power <- tibble(n.subj = currentSubj.N) %>%
+analyse_power <- tibble(n.subj = currentSubj.N, n.item = currentItem.N) %>%
   mutate(
-    tidy = map(n.subj, sim.power, n.sim = n_sim)
+    tidy = map(n.subj, sim.power, n.sim = n_sim, n.item)
   ) %>%
   unnest(tidy)
 
-write_csv(analyse_power, paste("results/direct_mod_power_analysis_fullData_final_", currentSubj.N, "subj_", n_iter,  "iter_", n_sim, "sim.csv", sep=""), append = T, col_names = T)
+write_csv(analyse_power, paste("results/direct_mod_power_analysis_fullData_final_", currentSubj.N, "subj_", currentItem.N, "items_", n_iter,  "iter_", n_sim, "sim.csv", sep=""), append = T, col_names = T)
 
 analyse_power %>%
   filter(key == "syntax_critical") %>%
   mutate(check_syntax = ifelse((lower > 0) | (upper < 0 ), 1, 0)) %>%
-  group_by(n.subj) %>%
+  group_by(n.subj, n.item) %>%
   summarise(power_syntax = mean(check_syntax)) -> analyse_power_summary
 
-analyse_power_summary %>% write_csv(paste("results/direct_mod_power_analysis_fullData_", currentSubj.N, "subj_", n_iter, "iter_", n_sim, "sim_summary.csv", sep = ""))
+analyse_power_summary %>% write_csv(paste("results/direct_mod_power_analysis_fullData_", currentSubj.N, "subj_", currentItem.N, "items_", n_iter, "iter_", n_sim, "sim_summary.csv", sep = ""))
