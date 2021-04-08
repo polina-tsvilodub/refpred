d3a94c58c34d1176fe6b3d413cca06220f416901
polina-tsvilodub <polina.tsvilodub@gmail.com>
2021-04-08 18:25:21 +0200

    remove multiple hits info

--- experiments/direct-modification/double-modification-FC/03_custom_views_templates.js
+++ experiments/direct-modification/double-modification-FC/03_custom_views_templates.js
@@ -660,9 +660,6 @@ const custom_intro_view = function(config) {
           <section class="magpie-text-container">
             <p class="magpie-view-text"> Thank you for taking part in our study. We are studying how people talk about things around them. The study will take about 5-7 minutes.<br /> <br />
 
-            <p class="magpie-view-text" style="font-family:Courier New, Courier"> Please note: There might be multiple posted versions of this HIT (name: <b>Animals - 111</b>) throughout the next few days.
-            Please attempt only one HIT in this series. You will not be allowed to complete multiple HITs in this series. </br> </br>
-            </p>
             <p class="magpie-view-text">
             By continuing, you are participating in an experiment being performed by cognitive scientists in the MIT Computational Psycholinguistics Lab. If you have questions about this research, please contact MH Tessler, at <a href="mailto:tessler@mit.edu">tessler@mit.edu</a>. You must be at least 18 years old to participate. Your participation in this research is voluntary. You may decline to answer any or all of the following questions. You may decline further participation, at any time, without adverse consequences. Your anonymity is assured; the researchers who have requested your participation will not receive any personal information about you.
              </p>
